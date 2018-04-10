52dad841ce4646e203b6d32cebad242a129bfa5c
jawauntb <Juan@Jawauns-MacBook-Pro.local>
2018-04-10 13:51:59 -0400

    cashreg class

---
+++ CashRegister.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; // 
+
+/*
+  J Brown 2016
+  Program to calculate the total expenses 
+ */
+
+public class CashRegister
+{
+    static int  totalExpenses = 0; //Total expenses
+    static int itemVal = 0; //Price of one item
+    public static Scanner keyboard = new Scanner(System.in);
+    static boolean done=false; //How long loop should go
+    static int count=0;
